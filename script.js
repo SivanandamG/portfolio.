29c5a232188eb97400537f517199d0db53f7731b
sivacreator <93380641+sivacreator@users.noreply.github.com>
2022-02-18 12:12:06 +0530

    .,.,..

--- script.js
+++ script.js
@@ -55,19 +55,19 @@ $(document).ready(function(){
         // autoplay: true,
         // autoplayTimeOut: 2000,
         // autoplayHoverPause: true,
-        // responsive: {
-        //     0:{
-        //         items: 1,
-        //         nav: false
-        //     },
-        //     300:{
-        //         items: 2,
-        //         nav: false
-        //     },
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            // 300:{
+            //     items: 2,
+            //     nav: false
+            // },
             // 1000:{
             //     items: 3,
             //     nav: false
             // }
-        // }
+        }
     });
 })
